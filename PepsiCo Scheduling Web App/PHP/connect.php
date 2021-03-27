363c10918de0e68a4cec5ccd12ce0dde0cf4b3ae
fmunoz1899 <81045295+fmunoz1899@users.noreply.github.com>
2021-03-26 23:07:25 -0400

    Update connect.php

--- PepsiCo Scheduling Web App/PHP/connect.php
+++ PepsiCo Scheduling Web App/PHP/connect.php
@@ -1,6 +1,6 @@
 //this is to connect to the database
 <?php
-	$link=mysqli_connect("localhost","root","","pepsi_databse");
+	$link=mysqli_connect("localhost","root","","pepsi_database");
 	if($link==false)
 		die("Could not connect to Database".mysqli_connect_error());
 ?>
