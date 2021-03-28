ef3f6e7a5a83d505328efa97a1a426d8721cbc37
JP2777 <81433619+JP2777@users.noreply.github.com>
2021-03-28 14:48:27 -0400

    Delete PepsiCo Scheduling Web App With PHP directory

--- PepsiCo Scheduling Web App With PHP/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-//this is to connect to the database
-
-	$link=mysqli_connect("localhost","root","","pepsi_database");
-	if($link==false)
-		die("Could not connect to Database".mysqli_connect_error());
-?>
