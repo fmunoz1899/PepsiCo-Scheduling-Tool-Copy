25b09a698b24b84b3ad16b047c24f90e76998a86
fmunoz1899 <81045295+fmunoz1899@users.noreply.github.com>
2021-04-04 14:26:36 -0400

    Update locations.php

--- PepsiCo Scheduling PHP/locations.php
+++ PepsiCo Scheduling PHP/locations.php
@@ -42,9 +42,9 @@ echo"
             <div class='row div_space'> 
                 <div class='col-md-1'> </div>
                 <div class='col-md-4'> 
-                    <label>Select location to filter results:</label>
+                    <label>Select location to filter results:</label> <!-- how exactly are we going to do the the filter for the locations -->
                     <select name='locations' id='lid'>
-						<option>Select one</option>";
+						<option>--Select one--</option>";
                         while($row=$result->fetch_assoc()) 
 							echo '<option value="'.$row["LocationID"].'">'.$row["LocationName"].'</option>';
 echo"
@@ -61,7 +61,31 @@ echo'
 	mysqli_close($link);
  ?>              
       <!--------------- Modal Code -------------->
+<?php
+	include('connect.php');
+	
+	if(isset($_POST['LocationName']) && isset($_POST['Address']) && isset($_POST['state']) && isset($_POST['ZipCode']) && isset($_POST['city']))
+	{
+		$cleanloc=filter_var($_POST['LocationName'], FILTER_SANITIZE_STRING);
+		$cleanadd=filter_var($_POST['Address'], FILTER_SANITIZE_STRING);
+		$cleanstate=filter_var($_POST['state'], FILTER_SANITIZE_STRING);
+		$cleanzip=filter_var($_POST['ZipCode'], FILTER_SANITIZE_NUMBER_INT);
+		$cleancity=filter_var($_POST['city'], FILTER_SANITIZE_STRING);
+		
+		if($cleanloc==$_POST['LocationName'] && $cleanadd==$_POST['Address'] && $cleanstate==$_POST['state'] && $cleanzip==$_POST['ZipCode'] && $cleancity==$_POST['city'])
+		{
+			$insert=$link->prepare("INSERT into location values(DEFAULT,?,?,?,?,?)");
+			$insert->bind_param("ssssi",$cleanloc,$cleanadd,$cleancity,$cleanstate,$cleanzip);
+			$insert->execute();
+		}
+		
+		else
+		{
+			echo"<script>alert('make sure you enter appropriate characters try again');</script>";
+		}
+	}
 
+?>
       <div class="modal fade" id="newLoc" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
         <div class="modal-dialog" role="document">
           <div class="modal-content">
@@ -72,18 +96,73 @@ echo'
               </button>
             </div>
             <div class="modal-body">
-              <form >
+              <form action="location.php" method="POST">
                 <p>Location Name: 
-                 <input type="text" name="LocationName">
+                 <input type="text" name="LocationName" required>
                 </p>
                 <p>Address: 
-                 <input type="text" name="Address">
+                 <input type="text" name="Address" required> <!-- ?make it so there are no 2 addresses the same? -->
                 </p>
+				<p>City:
+				<input type="text" name="city" required>
+				</p>
                 <p>State: 
-                    <input type="text" name="State">
+                <select name="state" id="state">
+				  <option value="AL" selected>Alabama</option>
+				  <option value="AK">Alaska</option>
+				  <option value="AZ">Arizona</option>
+				  <option value="AR">Arkansas</option>
+				  <option value="CA">California</option>
+				  <option value="CO">Colorado</option>
+				  <option value="CT">Connecticut</option>
+				  <option value="DE">Delaware</option>
+				  <option value="DC">District Of Columbia</option>
+				  <option value="FL">Florida</option>
+				  <option value="GA">Georgia</option>
+				  <option value="HI">Hawaii</option>
+				  <option value="ID">Idaho</option>
+				  <option value="IL">Illinois</option>
+				  <option value="IN">Indiana</option>
+				  <option value="IA">Iowa</option>
+				  <option value="KS">Kansas</option>
+				  <option value="KY">Kentucky</option>
+				  <option value="LA">Louisiana</option>
+				  <option value="ME">Maine</option>
+				  <option value="MD">Maryland</option>
+				  <option value="MA">Massachusetts</option>
+				  <option value="MI">Michigan</option>
+				  <option value="MN">Minnesota</option>
+				  <option value="MS">Mississippi</option>
+				  <option value="MO">Missouri</option>
+				  <option value="MT">Montana</option>
+				  <option value="NE">Nebraska</option>
+				  <option value="NV">Nevada</option>
+				  <option value="NH">New Hampshire</option>
+				  <option value="NJ">New Jersey</option>
+				  <option value="NM">New Mexico</option>
+				  <option value="NY">New York</option>
+				  <option value="NC">North Carolina</option>
+				  <option value="ND">North Dakota</option>
+				  <option value="OH">Ohio</option>
+				  <option value="OK">Oklahoma</option>
+				  <option value="OR">Oregon</option>
+				  <option value="PA">Pennsylvania</option>
+				  <option value="RI">Rhode Island</option>
+				  <option value="SC">South Carolina</option>
+				  <option value="SD">South Dakota</option>
+				  <option value="TN">Tennessee</option>
+				  <option value="TX">Texas</option>
+				  <option value="UT">Utah</option>
+				  <option value="VT">Vermont</option>
+				  <option value="VA">Virginia</option>
+				  <option value="WA">Washington</option>
+				  <option value="WV">West Virginia</option>
+				  <option value="WI">Wisconsin</option>
+				  <option value="WY">Wyoming</option>
+				</select>
                 </p>
                 <p>Zip Code: 
-                  <input type="text" name="ZipCode">
+                  <input type="text" name="ZipCode" minlength="5" maxlength="5" required> <!-- make it so it is just 5 numbers -->
                 </p>
 				<button type="submit" name="submit" class="btn btn-primary">Enter</button> 
               </form>
