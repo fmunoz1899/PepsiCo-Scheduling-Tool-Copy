33a2db11c95db725b86ca3534497e54ca948c019
fmunoz1899 <81045295+fmunoz1899@users.noreply.github.com>
2021-04-12 23:02:17 -0400

    Update Cal_View.php

--- PepsiCo Scheduling PHP/Cal_View.php
+++ PepsiCo Scheduling PHP/Cal_View.php
@@ -19,7 +19,7 @@
     </head>
 
     <body>
-      <nav class="navbar navbar-expand-sm fixed-top nav">
+      <nav class="navbar navbar-expand-sm">
         <ul class="navbar-nav">	
           <li class="nav-item active"><a class="nav-link" href="List_View.php">Schedule</a></li>
           <li class="nav-item active"><a class="nav-link a2" href="employees.php">Employees</a></li>
@@ -204,23 +204,24 @@
 <?php
 include('connect.php');
 date_default_timezone_set("America/New_York");
+
+		$curday=substr(date("r"),0,3); //this gets me the day abbrivated for the blockout
+		$curdate=date("Y-m-j"); //this gets the current date and formats it in yyy-mm-dd
 echo"
       <div class = 'row div_space '> 
         <div class = 'col-md-1'></div>
         <div class = 'col-md-10  cal_bg'>
         <div id='items'>";
 		
-		$curday=substr(date("r"),0,3); //this gets me the day abbrivated for the blockout
-		$empID="SELECT employeeID from employee";
-		//SELECT firstName, lastName, employee.employeeID, DayID, StartTime, EndTime FROM employee, ahours WHERE employee.employeeID=ahours.employeeID and StartTime=EndTime and StartTime!='NULL' and employee.employeeID=ahours.employeeID and DayID='".$curday."'
-		// used to show schedule of those working today	
-				
+		
+		$empID="SELECT employee.employeeID FROM employee, ahours WHERE employee.employeeID=ahours.employeeID and StartTime!='NULL' and EndTime!='NULL' and DayID='".$curday."'";
 		$result=mysqli_query($link,$empID);
 		
 		while($rowID=$result->fetch_assoc())
 		{
-			$count=0;
-			$time=date('08:00:00'); //starting time of the day
+			$counter=1; //for schedule cycling
+			$counter2=0; //for blokcout cycling
+			$time=date('06:00:00'); //starting time of the day
 			
 			$sched="SELECT firstName, lastName, employee.employeeID, DayID, StartTime, EndTime
 					FROM employee, ahours
@@ -234,13 +235,76 @@ echo"
               <tr>
                 <th><button type='button' data-toggle='modal' data-target='#newBO'>Blockout</button></th>
                 <th colspan='2'><b>".$rowsch['firstName']." ".$rowsch['lastName']."</b></th>";
-				while($time!='22:30:00') //time day ends plus the time increment
+				
+				$items="SELECT starttime, endtime FROM wi_schedule, workitem WHERE wi_schedule.ItemID=workitem.ItemID and workitem.employeeID=".$rowID['employeeID']." and wi_schedule.Date='".$curdate."' order by StartTime";
+				$result2=mysqli_query($link,$items);
+				$row = mysqli_fetch_array($result2);
+				
+				$ahours="SELECT StartTime, endtime FROM ahours WHERE ahours.EmployeeID=".$rowID['employeeID']." and ahours.DayID='".$curday."' order by StartTime";
+				$result3=mysqli_query($link,$ahours);
+				$rowahours = mysqli_fetch_array($result3);
+				
+				$blackout="SELECT starttime, endtime FROM blackout WHERE blackout.BDate='".$curdate."' and blackout.EmployeeID=".$rowID['employeeID']." order by StartTime";
+				$result4=mysqli_query($link,$blackout);
+				$rowblock = mysqli_fetch_array($result4);
+							
+				while($time!='22:00:00') //time day ends 
 				{
+					$entered=false; //if entered for schdule
+					
  echo"	            <tr class = 'row_height'>";
-					echo"<td class = 'hour'>".date('g:ia',strtotime($time))."</td> 
-					<td>	</td>
+					echo"<td class = 'hour'>".date('g:ia',strtotime($time))."</td>";
+					
+					
+					
+					if($time<$rowahours[0] || $time>=$rowahours[1]) //to show when an employee is or is not working
+					{
+						if(strtotime('+15 minutes',strtotime($rowahours[0]))==strtotime($rowahours[1]))
+							echo "<td class='no_work_15_min'>	</td>";
+						else
+							echo "<td class='no_work'>	</td>";
+						$entered=true;
+						$entered2=true;
+					}
+					
+					if($time>=$row[0] && $time<$row[1]) //to show when an employee has an appointment
+					{
+						if(strtotime('+15 minutes',strtotime($row[0]))==strtotime($row[1]))
+								echo "<td class='sched_work_15_min'>	</td>";
+							else
+								echo "<td class='sched_work'>	</td>";
+						$entered=true;
+					}
+					
+					else if(mysqli_data_seek($result2,$counter) && $time>$row[1]) //increments the appointments manually 
+					{
+						mysqli_data_seek($result2,$counter);
+						$row = mysqli_fetch_array($result2);
+						$counter+=1;
+					}
+					
+					if($time>=$rowblock[0] && $time<$rowblock[1] && !$entered) //to show when an employee has a blockout time 
+					{
+						if(strtotime('+15 minutes',strtotime($rowblock[0]))==strtotime($rowblock[1]))
+								echo "<td class='blockout_15_min'>	</td>";
+							else
+								echo "<td class='blockout'>	</td>";
+						$entered=true;
+					}
+					
+					else if(mysqli_data_seek($result4,$counter2) && $time>$rowblock[1]) //increments the appointments manually 
+					{
+						mysqli_data_seek($result4,$counter2);
+						$rowblock = mysqli_fetch_array($result4);
+						$counter2+=1;
+					}
+					
+					if(!$entered) //if there is nothing happening at a specific time while the employee is on the clock
+						echo "<td>	</td>
+										
 					</tr>";
-					$time = date('H:i:s',strtotime('+30 minutes',strtotime($time))); //to increment the schdule by 30 minutes
+					
+					$time = date('H:i:s',strtotime('+15 minutes',strtotime($time))); //to increment the schdule by 15 minutes
 				}
  echo"            
             </table>
