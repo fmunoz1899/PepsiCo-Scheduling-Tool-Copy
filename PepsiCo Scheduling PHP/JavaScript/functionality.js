d98c1869cd30ecbfbc0ec6d581c8756e6bc7c730
fmunoz1899 <81045295+fmunoz1899@users.noreply.github.com>
2021-04-24 21:57:47 -0400

    Update functionality.js

--- PepsiCo Scheduling PHP/JavaScript/functionality.js
+++ PepsiCo Scheduling PHP/JavaScript/functionality.js
@@ -2,30 +2,46 @@ $(document).ready(
 
     function()
     {
-
       $("#LV").click(
         function()
         {
             //alert("clicked");
             location.href = "List_View.php";
         });
+		
         $("#CV").click(
             function()
             {
                 location.href = "Cal_View.php";
             });
 			
-			$("#ELV").click(
+		$("#BV").click(
+            function()
+            {
+                location.href = "blocklist_view.php";
+            });
+			
+	   $("#ELV").click(
         function()
         {
             //alert("clicked");
             location.href = "empList_View.php";
         });
+		
         $("#ECV").click(
             function()
             {
                 location.href = "empCal_View.php";
             });
+			
+		$("#BCV").click(
+            function()
+            {
+                location.href = "empblocklist_View.php";
+            });
+			
+
+			
         $.fn.hScroll = function (amount) {
             amount = amount || 120;
             $(this).bind("DOMMouseScroll mousewheel", function (event) {
@@ -37,18 +53,31 @@ $(document).ready(
                 event.preventDefault();
             })
         };
+		$("#clear").click(
+				function()
+				{
+					document.getElementById("cal_form").reset();
+				});
+		
         $(document).ready(function() {
             $('#items').hScroll(60); 
         });
-		
 		
         $(document).ready(function() {		
             $('#datepicker').datepicker({
 				minDate: new Date("today")+-7,
 				dateFormat: "mm/dd/yy"
 			});
-			$('#datepicker').datepicker('setDate','today');		
-        });
+			$('#datepicker').datepicker('setDate',null);		
+        });	
+		
+		$(document).ready(function() {		
+            $('#datepicker2').datepicker({
+				minDate: new Date("today")+-7,
+				dateFormat: "mm/dd/yy"
+			});
+			$('#datepicker2').datepicker('setDate','today');		
+        });	
   
     });
  
