16b768d1c2ac326f0e41017fd2aeb2594598e535
JP2777 <81433619+JP2777@users.noreply.github.com>
2021-04-30 14:48:59 -0400

    Update filterblock_list.php

--- Bug/Bug/filterblock_list.php
+++ Bug/Bug/filterblock_list.php
@@ -82,7 +82,7 @@ date_default_timezone_set("America/New_York"); //timezone will need to change be
 	$searchfirst="%".$cleanfirst."%";
 	$searchlast="%".$cleanlast."%";
 	
-	$sqlWI=$link->prepare("SELECT firstName, lastName, startTime, endTime, bdate, reason, blackoutid
+	$sqlWI=$link->prepare("SELECT firstName, lastName, startTime, endTime, bdate, reason, blackoutid, blackout.EmployeeID
 			FROM blackout, employee, employeeprivlege
 			WHERE bdate='".$curdate."'  
 			and blackout.EmployeeID=employee.EmployeeID 
@@ -104,7 +104,9 @@ echo"
                 echo"<h1 class='h1_1'>Filtered results for ". date("n/j/y",strtotime($curdate))."</h1>";
 			else
 				echo"<h1 class='h1_1'>Filtered results for ".$cleanfirst." ".$cleanlast." on ". date("n/j/y",strtotime($curdate))."</h1>";
-			echo mysqli_num_rows($result)." result(s)";
+			echo "<div><center>";
+				echo mysqli_num_rows($result)." result(s)";
+			echo"</center></div>";
 			echo"
 		
             </div>";
@@ -143,29 +145,117 @@ echo"
 					<th> Reason </th>
 					<th> Date </th>
 				  </tr>";
-			while($row = $result->fetch_assoc()) {
-				echo "<tr>
-				<td><button>Edit</button></td>
-				<td><form method='POST' action='filterblock_list.php'>
-					<input name='id' type='hidden' value=".$row['blackoutid'].">
-					<input name='datepicker' type='hidden' value=".$curdate.">
-					<input name='filterfirst' type='hidden' value=".$cleanfirst.">
-					<input name='filterlast' type='hidden' value=".$cleanlast.">
-					<button>Remove</button></form></td>
-					
-					<td>".$row["firstName"]. " " . $row["lastName"] . "</td>
-					<td>" . date('g:ia',strtotime($row["startTime"]))."</td>
-					<td>" . date('g:ia',strtotime($row["endTime"]))."</td>
-					<td>" . $row['reason'] . "</td>
-					<td>" . date('n/j/Y',strtotime($row["bdate"]))."</td>
-				</tr>";
-			}
+				  while($row = $result->fetch_assoc()) 
+				  {
+					  $BID =  $row['blackoutid'];
+					  echo "<tr>
+						  <td><button class='btn btn-success EditBOFltr' type='button' data-toggle='modal' data-target='#EditBOFltr' value='" . $row['blackoutid'] . "'>Edit</button></td>
+						  <td><form method='POST' action='blocklist_view.php'><input name='id' type='hidden' value=".$row['blackoutid']."><button class = 'btn btn-danger'>Remove</button></form></td>
+						  <td id = ".$BID."name>".$row["firstName"]. " " . $row["lastName"] . "</td>
+						  <td id = ".$BID."sTime>" . date('g:ia',strtotime($row["startTime"]))."</td>
+						  <td id = ".$BID."eTime>" . date('g:ia',strtotime($row["endTime"]))."</td>
+						  <td id = ".$BID."reason>" . $row['reason'] . "</td>
+						  <td id = ".$BID."date>" . date('n/j/Y',strtotime($row["bdate"]))."</td>
+						  <td style = 'display: none' id = ".$BID."eid>".$row['EmployeeID']."</td>
+					  </tr>";
+				  }
 	echo'</div>';
 	mysqli_close($link);
 ?>
       <!------------------------------------->
 
+	  <!--------------- Modal Code to update Blockout Time-------------->
+	  <div class="modal fade" id="EditBOFltr" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title">Edit Blockout</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+			<div class = row>
+				<div class = "col-md-2"></div>
+				<div class = "col-md-9">
+					<div class="modal-body">
+					<form action = "blocklist_view.php" method = "POST">
+					<div class="form-group">
+						<label>Start Time</label>
+						<select  required>
+						<option name='startTime' id='startTime' value='' selected>--Time--</option>
+						<?php
+							$time=date('06:00:00');
+							while($time!='22:00:00') //time day ends 
+							{
+								echo"<option value='".$time."'>". date('g:ia',strtotime($time))."</option>";
+								$time = date('H:i:s',strtotime('+15 minutes',strtotime($time)));
+							}
+						?>
+						</select>
+					</div>
+							
+					<div class="form-group">
+						<label>End Time</label>
+						<select  required>
+						<option name='endTime' id='endTime' value='' selected>--Time--</option>
+						<?php
+							$time=date('06:15:00');
+							while($time!='22:15:00') //time day ends 
+							{
+								echo"<option value='".$time."'>". date('g:ia',strtotime($time))."</option>";
+								$time = date('H:i:s',strtotime('+15 minutes',strtotime($time)));
+							}
+						?>
+					</select>
+					</div>  
+					<div class="form-group">
+						<label>Date</label>
+						<input name="datepicker6" type="text" id="datepicker6" readonly='true'>
+					</div>
+					<div class="form-group">
+						<label>All</label>
+						<input type="checkbox" id="forallEdit" name="forallEdit" disabled>
+					</div>
+					<div class="form-group">
+						<label>Who</label>
+						<select   disabled required>
+						<option name='who' id='whoEdit' value=''  selected>--Select--</option>
+						<?php
+							include('connect.php');
+							$names="SELECT firstname, lastname, employee.employeeid
+									FROM employee, employeeprivlege
+									WHERE privilegeid='E'
+									and employee.employeeid=employeeprivlege.employeeid
+									ORDER BY firstname";
+							$result=$link->query($names);
+							while($row = $result->fetch_assoc()) 
+							{
+								echo"<option value=".$row['employeeid'].">".$row['firstname']." ".$row['lastname']."</option>";
+							}
 
+						?>
+						</select>
+					</div>
+					<div class="form-group">
+						<label>Reason</label>
+						<input type="text" name="reasonEdit" id="reasonEdit">
+					</div>
+					<input type = "hidden" id = "EmpID"  required>
+					<input type = "hidden" id = "BOtimeID"  required>
+						<button type="submit" name="submit" class="btn btn-primary">Edit</button> 
+					</form>
+					
+				</div>
+				<div class="modal-footer">
+				<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+				</div>
+            </div>
+          </div>
+        </div>
+      </div>
+	  
+
+      <!------------------------------------->
             
     </body>
 </html>
