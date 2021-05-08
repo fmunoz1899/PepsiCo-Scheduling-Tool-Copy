986f14d0edb9e83d6876574552eff181700be3ce
kgilmore2 <81122965+kgilmore2@users.noreply.github.com>
2021-05-08 11:13:55 -0400

    Update adminfilterloc.php

--- PepsiCo/adminfilterloc.php
+++ PepsiCo/adminfilterloc.php
@@ -86,7 +86,7 @@ if(isset($_SESSION['admin']) && $_SESSION['admin']===true)
         <div class="modal-dialog" role="document">
           <div class="modal-content">
             <div class="modal-header">
-              <h5 class="modal-title">Create New Location</h5>
+              <h5 class="modal-title">Edit Existing Location</h5>
               <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span> </button>
             </div>
             <div class="modal-body modalContent">
